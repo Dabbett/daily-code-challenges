b31cebb8ce58ce25be55e3e9acbaa09bca257be8
Dabbett <abbettdylan@gmail.com>
2023-10-12 16:15:43 -0400

    add kata

--- script.js
+++ script.js
@@ -1116,3 +1116,19 @@
 //     return "white"
 //   }
 // }
+
+// In this Kata you have to find the factors of integer down to the limit including the limiting number. There will be no negative numbers. Return the result as an array of numbers in ascending order.
+
+// If the limit is more than the integer, return an empty list
+
+// As a challenge, see if you can do it in one line
+
+// function factors(integer, limit){
+//   let arr = []
+//   for(let i = integer; i >= limit; i--){
+//     if(integer % i === 0){
+//       arr.push(i)
+//       }
+//   }
+//   return arr.sort((a,b) => a - b)
+// }
