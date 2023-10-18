3663a5b329c9f0e5970c465e5f50df104648bb41
Dabbett <abbettdylan@gmail.com>
2023-10-18 14:34:40 -0400

    codewars problem

--- script.js
+++ script.js
@@ -1175,3 +1175,17 @@
 // else {
 // return false;}
 // }
+
+
+//If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+//const countSheep = (num) => {
+//     let arr = [];
+//     for (let i = 1; i <= num; i++) {
+//     arr.push(`${i} sheep...`);  
+//     }
+//     return arr.join('');
+// };
