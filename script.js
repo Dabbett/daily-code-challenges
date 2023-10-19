611490d64dee6bf945670b2ae29d0fd0b123fc47
Dabbett <abbettdylan@gmail.com>
2023-10-19 14:26:09 -0400

    codewars 8w

--- script.js
+++ script.js
@@ -1195,3 +1195,12 @@
 // function DNAtoRNA(dna){
 //   return dna.replace(/T/g, 'U');
 // }
+
+//Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+
+// function past(h, m, s){
+//   return h * 3600000 + m * 60000  + s * 1000;
+// }
