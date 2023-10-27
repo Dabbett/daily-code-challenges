0185eca22ac3a0d82bf29c202761f4b685c93f47
Dabbett <abbettdylan@gmail.com>
2023-10-26 20:55:00 -0400

    kata

--- script.js
+++ script.js
@@ -1342,3 +1342,22 @@
 //   }}
 //   return arr
 // }
+
+
+///make a very inneficient way ofdoing the following: 
+
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+
+// function getDivisorsCnt(n){
+//   let arr =[]
+//    for(let i = 0; i <= n; i++){
+//      if (n % i === 0){
+//        arr.push(i)
+//      }
+//    }
+//   return arr.length
+// }
