b7d29cc4ed82df4c217e941e941c09f692aa0010
Dabbett <abbettdylan@gmail.com>
2023-11-01 09:51:54 -0400

    kata

--- script.js
+++ script.js
@@ -1438,3 +1438,7 @@
 //   }
 //   return table;
 // }
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error"
+
+// const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
