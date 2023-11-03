d76d115f21781364135ef566f956434eb78f1d1a
Dabbett <abbettdylan@gmail.com>
2023-11-03 10:56:54 -0400

    kata

--- script.js
+++ script.js
@@ -1592,3 +1592,24 @@
 //  let indexOfHighest = arr2.indexOf(Math.max(...arr2))
 //  return arr[indexOfHighest]
 //  }
+
+// Your task is to write a function which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+
+
+// const sequenceSum = (begin, end, step) => {
+//   let total = 0;
+  
+//   if(begin > end) {
+//     return 0
+//   } else {
+//     for(let i = begin; i <= end; i += step) {
+//       total += i;
+//     }
+//   return total
+//   }
+//};
