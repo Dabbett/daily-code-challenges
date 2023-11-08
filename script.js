2bc4ca26e95716a0aadbc7666fc8062049fc8cab
Dabbett <abbettdylan@gmail.com>
2023-11-08 13:25:19 -0500

    6k kata

--- script.js
+++ script.js
@@ -1671,3 +1671,17 @@
 // function apple(x) {
 //   return x**2 > 1000 ? "It's hotter than the sun!!" : 'Help yourself to a honeycomb Yorkie for the glovebox.';
 // }
+
+// function likes(names) {
+//   if(names.length === 0 ){
+//     return `no one likes this`
+//   }else if (names.length == 1){
+//     return `${names[0]} likes this`
+//   }else if (names.length === 2){
+//     return `${names[0]} and ${names[1]} like this`
+//   }else if (names.length === 3){
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//   }else {
+//     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+//   }
+// }
