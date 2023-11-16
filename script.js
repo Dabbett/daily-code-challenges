699dbad5fdd1d124007c1aaa0b26a75de8b844a4
Dabbett <abbettdylan@gmail.com>
2023-11-16 09:36:45 -0500

    kata

--- script.js
+++ script.js
@@ -1742,3 +1742,8 @@
 //   let joined = [...arr1,...filteredArr2]
 //   return joined.sort((a,b)=> a-b)
 // }
+
+// function highAndLow(numbers){
+//   numbers = numbers.split(' ').sort((a,b)=> a - b)
+//   return `${numbers[numbers.length -1]} ${numbers[0]}`
+// }
