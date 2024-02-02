577651f57a7fa5fa81af76e4869c2260da8082d3
Dabbett <abbettdylan@gmail.com>
2024-02-02 12:32:06 -0500

    kata

--- script.js
+++ script.js
@@ -2034,4 +2034,11 @@
 //   return sum;
 // }
 
-//
+//function positiveSum(arr) {
+//   let sum = 0
+//   for(let i =0; i < arr.length; i++) {
+//     if(arr[i] >0) {
+//        sum += arr[i]
+//        }
+//   } return sum
+// }
