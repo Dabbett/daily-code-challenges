4f6d5dc0768e1f0be8104d42a5141c3a23808a30
Dabbett <abbettdylan@gmail.com>
2024-03-18 09:10:26 -0400

    kata

--- script.js
+++ script.js
@@ -2394,12 +2394,12 @@
 
 //---------
 
-Add the value "codewars" to the websites array.
-After your code executes the websites array should == ["codewars"]
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
 
-The websites array has already been defined for you using the following code:
+// The websites array has already been defined for you using the following code:
 
-var websites = [];
+// var websites = [];
 
 
-websites[0] = "codewars";
+// websites[0] = "codewars";
