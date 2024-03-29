411b7ec7eb22ef9ee70273fdbb5d0fa918580a89
Dylan Abbett <abbettdylan@gmail.com>
2024-03-29 13:12:26 -0400

    interface refresh

--- altScript.ts
+++ altScript.ts
@@ -5,9 +5,29 @@ arrayOfNames[2] = 'Amber Hudson'
 
 console.log(arrayOfNames)
 
+
 // when running, compiles to JS: 
 // var arrayOfNames = [];
 // arrayOfNames[0] = 'Ron Franklin';
 // arrayOfNames.push('Sam Costa');
 // arrayOfNames[2] = 'Amber Hudson';
 // console.log(arrayOfNames);
+
+
+export interface EmployeeInterface {
+    name: string;
+    age: number;
+    sex: string;
+}
+
+export class Employee {
+    name: string
+    age: number
+    sex: string
+
+    constructor(employeeData: EmployeeInterface) {
+        this.name = employeeData.name
+        this.age = employeeData.age
+        this.sex = employeeData.sex
+    }
+}
