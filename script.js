9b3d88baa1c97f8bff2ca50d8ad4f7c6185a48ff
Dylan Abbett <abbettdylan@gmail.com>
2024-04-01 11:31:21 -0400

    kata: array method practice

--- script.js
+++ script.js
@@ -2673,9 +2673,33 @@
 //     backgroundColor: 'white'
 // }
 
-function betterThanAverage(classPoints, yourPoints) {
-    let average = classPoints.reduce((a,b) => a+b) / classPoints.length
+// function betterThanAverage(classPoints, yourPoints) {
+//     let average = classPoints.reduce((a,b) => a+b) / classPoints.length
     
-    return yourPoints > average
-  }
+//     return yourPoints > average
+//   }
   
+
+function getLength(arr){
+    //return length of arr
+    return arr.length
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return arr[0]
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return arr[getLength(arr)-1]
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    arr.push(el)
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    arr.pop()
+    return arr
+  }
