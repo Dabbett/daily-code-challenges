5012d84e987f4554fd996989778e5b03ed935fe6
Dylan Abbett <abbettdylan@gmail.com>
2024-04-10 08:20:59 -0400

    presentational vs functional components

--- altScript.js
+++ altScript.js
@@ -9,3 +9,51 @@
 // // arrayOfNames.push('Sam Costa');
 // // arrayOfNames[2] = 'Amber Hudson';
 // // console.log(arrayOfNames);
+
+
+// PresentationalComponent.js
+import React, { Component } from 'react';
+
+class PresentationalComponent extends Component {
+  render() {
+    const { message } = this.props;
+    return (
+      <div>
+        <h1>Presentational Component</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+}
+
+export default PresentationalComponent;
+
+
+// ContainerComponent.js
+import React, { Component } from 'react';
+import PresentationalComponent from './PresentationalComponent';
+
+class ContainerComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Hello from Container!'
+    };
+  }
+
+  handleButtonClick = () => {
+    this.setState({ message: 'Updated message from Container!' });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Container Component</h1>
+        <button onClick={this.handleButtonClick}>Update Message</button>
+        <PresentationalComponent message={this.state.message} />
+      </div>
+    );
+  }
+}
+
+export default ContainerComponent;
