00eea8557b4e96e761334d4874bf8a930be90097
Dylan Abbett <abbettdylan@gmail.com>
2024-06-03 16:50:58 -0400

    kata

--- script.py
+++ script.py
@@ -7,3 +7,10 @@ class Hero(object):
         self.health = 100
         self.damage = 5
         self.experience = 0
+
+
+# python funtion multiplier
+def multiply(n):
+    num = len(str(abs(n)))
+    multiplier = 5 ** num
+    return  n * multiplier
